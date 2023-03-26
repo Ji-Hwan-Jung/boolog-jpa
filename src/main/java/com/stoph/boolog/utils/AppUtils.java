b1b50979fe4f1ec43e94d573117de0329de7d407
jjh <stoph.dev@gmail.com>
2023-03-26 16:16:33 +0900

    비밀번호 생성 로직 개선

--- src/main/java/com/stoph/boolog/utils/AppUtils.java
+++ src/main/java/com/stoph/boolog/utils/AppUtils.java
@@ -10,16 +10,17 @@ public class AppUtils {
 
     //대소문자의 영문으로 이루어진 랜덤 문자열 반환
     public static String createPassword() {
-        StringBuffer password = new StringBuffer("");
+        StringBuffer password = new StringBuffer();
         SecureRandom sr = new SecureRandom();
-        int cnt = 1;
+        int cnt = 0;
 
         while (cnt != 10) {
-            int num = (int) ((sr.nextDouble() * 57) + 65);
-            if (num < 91 || num > 96) {
-                password.append((char) num);
-                cnt += 1;
+            int num = (int) ((sr.nextDouble() * 53) + 65);
+            if (num > 90 && num < 97) {
+                num = (num%2 != 0)? num - 26 : num + 26;
             }
+            password.append((char) num);
+            cnt++;
         }
         return password.toString();
     }
